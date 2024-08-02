ba0411ba8c8f9e61d100f80d04069cdc7a47636c
Gautam-121 <gautamdhakate1234@gmail.com>
2024-08-02 10:37:29 +0530

    add specific frontend url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 // Enable CORS with credentials
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500',"http://localhost:5173" , "http://localhost:8081","https://cookies-frontend.vercel.app/"];
+const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500',"http://localhost:5173" , "http://localhost:8081","https://cookies-frontend.vercel.app"];
 app.use(cors({
   origin: (origin, callback) => {
     console.log(origin)
